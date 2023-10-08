f3e0eabd9d11a61fd247e7b0a0eaaaacf5a0c4e1
root <alefsoftware4@gmail.com>
2023-10-08 14:25:43 +0200

    add autoexec

---
+++ autoexec.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd /var/www/html &&
+composer update && composer dump-autoload &&
+chmod 777 -R storage bootstrap/cache &&
+php artisan optimize:clear &&
+php artisan optimize &&
+php artisan migrate --force --seed
