b1c13feb967b215cb4aa5660ae3c3b937c6969d0
MohamedNabilFarouk <57891695+MohamedNabilFarouk@users.noreply.github.com>
2023-10-16 17:29:56 +0300

    Update autoexec.sh

--- autoexec.sh
+++ autoexec.sh
@@ -4,4 +4,5 @@ composer update && composer dump-autoload &&
 chmod 777 -R storage bootstrap/cache &&
 php artisan optimize:clear &&
 php artisan optimize &&
+php artisan storage:link &&
 php artisan migrate --force --seed
