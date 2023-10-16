5210ac7059e7157321f11b6fdcba2bf2cf50b0db
mohamednabilfarouk <mohamednabilfarouk@gmail.com>
2023-10-16 11:44:10 +0300

    upload

--- app/Interfaces/front/IndexRepositoryInterface.php
+++ app/Interfaces/front/IndexRepositoryInterface.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Interfaces\Front;
+namespace App\Interfaces\front;
 
 interface IndexRepositoryInterface
 {
--- resources/views/front/layouts/footer.blade.php
+++ resources/views/front/layouts/footer.blade.php
@@ -15,7 +15,7 @@
                                 <button class="btn" type="submit">Subscribe</button>
                             </form>
                         </div>
-                        <img src="{{asset('Front/assets/imgs/banner/banner-9.png')}}" alt="newsletter" />
+                        <img src="{{asset('front/assets/imgs/banner/banner-9.png')}}" alt="newsletter" />
                     </div>
                 </div>
             </div>
@@ -27,7 +27,7 @@
                 <div class="col-lg-1-5 col-md-4 col-12 col-sm-6 mb-md-4 mb-xl-0">
                     <div class="banner-left-icon d-flex align-items-center wow animate__animated animate__fadeInUp" data-wow-delay="0">
                         <div class="banner-icon">
-                            <img src="{{asset('Front/assets/imgs/theme/icons/icon-1.svg')}}" alt="" />
+                            <img src="{{asset('front/assets/imgs/theme/icons/icon-1.svg')}}" alt="" />
                         </div>
                         <div class="banner-text">
                             <h3 class="icon-box-title">Best prices & offers</h3>
@@ -38,7 +38,7 @@
                 <div class="col-lg-1-5 col-md-4 col-12 col-sm-6">
                     <div class="banner-left-icon d-flex align-items-center wow animate__animated animate__fadeInUp" data-wow-delay=".1s">
                         <div class="banner-icon">
-                            <img src="{{asset('Front/assets/imgs/theme/icons/icon-2.svg')}}" alt="" />
+                            <img src="{{asset('front/assets/imgs/theme/icons/icon-2.svg')}}" alt="" />
                         </div>
                         <div class="banner-text">
                             <h3 class="icon-box-title">Free delivery</h3>
@@ -49,7 +49,7 @@
                 <div class="col-lg-1-5 col-md-4 col-12 col-sm-6">
                     <div class="banner-left-icon d-flex align-items-center wow animate__animated animate__fadeInUp" data-wow-delay=".2s">
                         <div class="banner-icon">
-                            <img src="{{asset('Front/assets/imgs/theme/icons/icon-3.svg')}}" alt="" />
+                            <img src="{{asset('front/assets/imgs/theme/icons/icon-3.svg')}}" alt="" />
                         </div>
                         <div class="banner-text">
                             <h3 class="icon-box-title">Great daily deal</h3>
@@ -60,7 +60,7 @@
                 <div class="col-lg-1-5 col-md-4 col-12 col-sm-6">
                     <div class="banner-left-icon d-flex align-items-center wow animate__animated animate__fadeInUp" data-wow-delay=".3s">
                         <div class="banner-icon">
-                            <img src="{{asset('Front/assets/imgs/theme/icons/icon-4.svg')}}" alt="" />
+                            <img src="{{asset('front/assets/imgs/theme/icons/icon-4.svg')}}" alt="" />
                         </div>
                         <div class="banner-text">
                             <h3 class="icon-box-title">Wide assortment</h3>
@@ -71,7 +71,7 @@
                 <div class="col-lg-1-5 col-md-4 col-12 col-sm-6">
                     <div class="banner-left-icon d-flex align-items-center wow animate__animated animate__fadeInUp" data-wow-delay=".4s">
                         <div class="banner-icon">
-                            <img src="{{asset('Front/assets/imgs/theme/icons/icon-5.svg')}}" alt="" />
+                            <img src="{{asset('front/assets/imgs/theme/icons/icon-5.svg')}}" alt="" />
                         </div>
                         <div class="banner-text">
                             <h3 class="icon-box-title">Easy returns</h3>
@@ -82,7 +82,7 @@
                 <div class="col-lg-1-5 col-md-4 col-12 col-sm-6 d-xl-none">
                     <div class="banner-left-icon d-flex align-items-center wow animate__animated animate__fadeInUp" data-wow-delay=".5s">
                         <div class="banner-icon">
-                            <img src="{{asset('Front/assets/imgs/theme/icons/icon-6.svg')}}" alt="" />
+                            <img src="{{asset('front/assets/imgs/theme/icons/icon-6.svg')}}" alt="" />
                         </div>
                         <div class="banner-text">
                             <h3 class="icon-box-title">Safe delivery</h3>
@@ -99,14 +99,14 @@
                 <div class="col">
                     <div class="widget-about font-md mb-md-3 mb-lg-3 mb-xl-0 wow animate__animated animate__fadeInUp" data-wow-delay="0">
                         <div class="logo mb-30">
-                            <a href="index.html" class="mb-15"><img src="{{asset('Front/assets/imgs/theme/logo.svg')}}" alt="logo" /></a>
+                            <a href="index.html" class="mb-15"><img src="{{asset('front/assets/imgs/theme/logo.svg')}}" alt="logo" /></a>
                             <p class="font-lg text-heading">Awesome grocery store website template</p>
                         </div>
                         <ul class="contact-infor">
-                            <li><img src="{{asset('Front/imgs/theme/icons/icon-location.svg')}}" alt="" /><strong>Address: </strong> <span>5171 W Campbell Ave undefined Kent, Utah 53127 United States</span></li>
-                            <li><img src="{{asset('Front/assets/imgs/theme/icons/icon-contact.svg')}}" alt="" /><strong>Call Us:</strong><span>(+91) - 540-025-124553</span></li>
-                            <li><img src="{{asset('Front/assets/imgs/theme/icons/icon-email-2.svg')}}" alt="" /><strong>Email:</strong><span>sale@Nest.com</span></li>
-                            <li><img src="{{asset('Front/assets/imgs/theme/icons/icon-clock.svg')}}" alt="" /><strong>Hours:</strong><span>10:00 - 18:00, Mon - Sat</span></li>
+                            <li><img src="{{asset('front/imgs/theme/icons/icon-location.svg')}}" alt="" /><strong>Address: </strong> <span>5171 W Campbell Ave undefined Kent, Utah 53127 United States</span></li>
+                            <li><img src="{{asset('front/assets/imgs/theme/icons/icon-contact.svg')}}" alt="" /><strong>Call Us:</strong><span>(+91) - 540-025-124553</span></li>
+                            <li><img src="{{asset('front/assets/imgs/theme/icons/icon-email-2.svg')}}" alt="" /><strong>Email:</strong><span>sale@Nest.com</span></li>
+                            <li><img src="{{asset('front/assets/imgs/theme/icons/icon-clock.svg')}}" alt="" /><strong>Hours:</strong><span>10:00 - 18:00, Mon - Sat</span></li>
                         </ul>
                     </div>
                 </div>
@@ -162,11 +162,11 @@
                 <h4 class="widget-title">Install App</h4>
                 <p class="">From App Store or Google Play</p>
                 <div class="download-app">
-                    <a href="#" class="hover-up mb-sm-2 mb-lg-0"><img class="active" src="{{asset('Front/assets/imgs/theme/app-store.jpg')}}" alt="" /></a>
-                    <a href="#" class="hover-up mb-sm-2"><img src="{{asset('Front/assets/imgs/theme/google-play.jpg')}}" alt="" /></a>
+                    <a href="#" class="hover-up mb-sm-2 mb-lg-0"><img class="active" src="{{asset('front/assets/imgs/theme/app-store.jpg')}}" alt="" /></a>
+                    <a href="#" class="hover-up mb-sm-2"><img src="{{asset('front/assets/imgs/theme/google-play.jpg')}}" alt="" /></a>
                 </div>
                 <p class="mb-20">Secured Payment Gateways</p>
-                <img class="" src="{{asset('Front/assets/imgs/theme/payment-method.png')}}" alt="" />
+                <img class="" src="{{asset('front/assets/imgs/theme/payment-method.png')}}" alt="" />
             </div>
         </div>
     </section>
@@ -180,22 +180,22 @@
             </div>
             <div class="col-xl-4 col-lg-6 text-center d-none d-xl-block">
                 <div class="hotline d-lg-inline-flex mr-30">
-                    <img src="{{asset('Front/assets/imgs/theme/icons/phone-call.svg')}}" alt="hotline" />
+                    <img src="{{asset('front/assets/imgs/theme/icons/phone-call.svg')}}" alt="hotline" />
                     <p>1900 - 6666<span>Working 8:00 - 22:00</span></p>
                 </div>
                 <div class="hotline d-lg-inline-flex">
-                    <img src="{{asset('Front/assets/imgs/theme/icons/phone-call.svg')}}" alt="hotline" />
+                    <img src="{{asset('front/assets/imgs/theme/icons/phone-call.svg')}}" alt="hotline" />
                     <p>1900 - 8888<span>24/7 Support Center</span></p>
                 </div>
             </div>
             <div class="col-xl-4 col-lg-6 col-md-6 text-end d-none d-md-block">
                 <div class="mobile-social-icon">
                     <h6>Follow Us</h6>
-                    <a href="#"><img src="{{asset('Front/assets/imgs/theme/icons/icon-facebook-white.svg')}}" alt="" /></a>
-                    <a href="#"><img src="{{asset('Front/assets/imgs/theme/icons/icon-twitter-white.svg')}}" alt="" /></a>
-                    <a href="#"><img src="{{asset('Front/assets/imgs/theme/icons/icon-instagram-white.svg')}}" alt="" /></a>
-                    <a href="#"><img src="{{asset('Front/assets/imgs/theme/icons/icon-pinterest-white.svg')}}" alt="" /></a>
-                    <a href="#"><img src="{{asset('Front/assets/imgs/theme/icons/icon-youtube-white.svg')}}" alt="" /></a>
+                    <a href="#"><img src="{{asset('front/assets/imgs/theme/icons/icon-facebook-white.svg')}}" alt="" /></a>
+                    <a href="#"><img src="{{asset('front/assets/imgs/theme/icons/icon-twitter-white.svg')}}" alt="" /></a>
+                    <a href="#"><img src="{{asset('front/assets/imgs/theme/icons/icon-instagram-white.svg')}}" alt="" /></a>
+                    <a href="#"><img src="{{asset('front/assets/imgs/theme/icons/icon-pinterest-white.svg')}}" alt="" /></a>
+                    <a href="#"><img src="{{asset('front/assets/imgs/theme/icons/icon-youtube-white.svg')}}" alt="" /></a>
                 </div>
                 <p class="font-sm">Up to 15% discount on your first subscribe</p>
             </div>
@@ -207,34 +207,34 @@
     <div class="preloader d-flex align-items-center justify-content-center">
         <div class="preloader-inner position-relative">
             <div class="text-center">
-                <img src="{{asset('Front/assets/imgs/theme/loading.gif')}}" alt="" />
+                <img src="{{asset('front/assets/imgs/theme/loading.gif')}}" alt="" />
             </div>
         </div>
     </div>
 </div>
 <!-- Vendor JS-->
-<script src="{{asset('Front/assets/js/vendor/modernizr-3.6.0.min.js')}}"></script>
-<script src="{{asset('Front/assets/js/vendor/jquery-3.6.0.min.js')}}"></script>
-<script src="{{asset('Front/assets/js/vendor/jquery-migrate-3.3.0.min.js')}}"></script>
-<script src="{{asset('Front/assets/js/vendor/bootstrap.bundle.min.js')}}"></script>
-<script src="{{asset('Front/assets/js/plugins/slick.js')}}"></script>
-<script src="{{asset('Front/assets/js/plugins/jquery.syotimer.min.js')}}"></script>
-<script src="{{asset('Front/assets/js/plugins/waypoints.js')}}"></script>
-<script src="{{asset('Front/assets/js/plugins/wow.js')}}"></script>
-<script src="{{asset('Front/assets/js/plugins/perfect-scrollbar.js')}}"></script>
-<script src="{{asset('Front/assets/js/plugins/magnific-popup.js')}}"></script>
-<script src="{{asset('Front/assets/js/plugins/select2.min.js')}}"></script>
-<script src="{{asset('Front/assets/js/plugins/counterup.js')}}"></script>
-<script src="{{asset('Front/assets/js/plugins/jquery.countdown.min.js')}}"></script>
-<script src="{{asset('Front/assets/js/plugins/images-loaded.js')}}"></script>
-<script src="{{asset('Front/assets/js/plugins/isotope.js')}}"></script>
-<script src="{{asset('Front/assets/js/plugins/scrollup.js')}}"></script>
-<script src="{{asset('Front/assets/js/plugins/jquery.vticker-min.js')}}"></script>
-<script src="{{asset('Front/assets/js/plugins/jquery.theia.sticky.js')}}"></script>
-<script src="{{asset('Front/assets/js/plugins/jquery.elevatezoom.js')}}"></script>
+<script src="{{asset('front/assets/js/vendor/modernizr-3.6.0.min.js')}}"></script>
+<script src="{{asset('front/assets/js/vendor/jquery-3.6.0.min.js')}}"></script>
+<script src="{{asset('front/assets/js/vendor/jquery-migrate-3.3.0.min.js')}}"></script>
+<script src="{{asset('front/assets/js/vendor/bootstrap.bundle.min.js')}}"></script>
+<script src="{{asset('front/assets/js/plugins/slick.js')}}"></script>
+<script src="{{asset('front/assets/js/plugins/jquery.syotimer.min.js')}}"></script>
+<script src="{{asset('front/assets/js/plugins/waypoints.js')}}"></script>
+<script src="{{asset('front/assets/js/plugins/wow.js')}}"></script>
+<script src="{{asset('front/assets/js/plugins/perfect-scrollbar.js')}}"></script>
+<script src="{{asset('front/assets/js/plugins/magnific-popup.js')}}"></script>
+<script src="{{asset('front/assets/js/plugins/select2.min.js')}}"></script>
+<script src="{{asset('front/assets/js/plugins/counterup.js')}}"></script>
+<script src="{{asset('front/assets/js/plugins/jquery.countdown.min.js')}}"></script>
+<script src="{{asset('front/assets/js/plugins/images-loaded.js')}}"></script>
+<script src="{{asset('front/assets/js/plugins/isotope.js')}}"></script>
+<script src="{{asset('front/assets/js/plugins/scrollup.js')}}"></script>
+<script src="{{asset('front/assets/js/plugins/jquery.vticker-min.js')}}"></script>
+<script src="{{asset('front/assets/js/plugins/jquery.theia.sticky.js')}}"></script>
+<script src="{{asset('front/assets/js/plugins/jquery.elevatezoom.js')}}"></script>
 <!-- Template  JS -->
-<script src="{{asset('Front/assets/js/main.js?v=5.6')}}"></script>
-<script src="{{asset('Front/assets/js/shop.js?v=5.6')}}"></script>
+<script src="{{asset('front/assets/js/main.js?v=5.6')}}"></script>
+<script src="{{asset('front/assets/js/shop.js?v=5.6')}}"></script>
 </body>
 
 </html>
